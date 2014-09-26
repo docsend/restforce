6b2030753e9a909ef30beac100d291104ddbbc31
David Haley <david@docsend.com>
2014-09-25 17:31:29 -0700

    Add matcher descriptions to pacify rspec
    if the tests fail without these matchers, rspec outputs very angry
    warning messages about missing descriptions. It's annoying so shut it
    up with these...

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,10 +2,18 @@ RSpec::Matchers.define :include_picklist_values do |expected|
   match do |actual|
     actual.all? { |picklist_value| expected.include? picklist_value['value'] }
   end
+
+  description do
+   "include picklist values"
+ end
 end
 
 RSpec::Matchers.define :have_client do |expected|
   match do |actual|
     actual.instance_variable_get(:@client) == expected
+  end
+
+  description do
+    "have client"
   end
 end
