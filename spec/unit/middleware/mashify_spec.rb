196cd5da4d3f1d750abe1dbd4394847075c385af
David Haley <david@docsend.com>
2014-09-25 17:35:14 -0700

    Replace middleware calling with direct parsing
    I don't feel like constructing a whole app+call+on_complete stub, so
    forget it and directly parse the body

--- spec/unit/middleware/mashify_spec.rb
+++ spec/unit/middleware/mashify_spec.rb
@@ -4,7 +4,7 @@ describe Restforce::Middleware::Mashify do
   let(:env) { { :body => JSON.parse(fixture('sobject/query_success_response')) } }
 
   describe '.call' do
-    subject { lambda { middleware.call(env) } }
+    subject { lambda { env[:body] = Restforce::Middleware::Mashify.new().parse(env[:body]) } }
 
     it { should change { env[:body] }.to(kind_of(Restforce::Collection)) }
   end
